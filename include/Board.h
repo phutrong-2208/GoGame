0e441a06422c9d7a6a72cdabbd1b5901182b9575
phutrong-2208 <tronghuy356@gmail.com>
2025-10-16 21:25:48 +0700

    test

---
+++ include/Board.h
@@ -0,0 +1,14 @@
+
+#define BOARD_H
+
+const int  BOARD_SIZE = 9;
+
+enum Piece {Black, White, Empty};
+
+class GoBoard{
+    public:
+        Piece grid[BOARD_SIZE][BOARD_SIZE];
+    private:
+        bool ended(void);
+        bool newStep(int x, int y, Piece color);
+};
---
+++ src/Board.cpp
@@ -0,0 +1,19 @@
+#include <Board.h>
+
+//abcxyz
+bool GoBoard :: ended(void){
+    for (int i = 0; i < 9; ++i) for (int j = 0; j < 9; ++j){
+          GoBoard :: grid[i][j] = Empty;
+    }
+}
+
+bool inside(int x, int y){
+    return x >= 0 and x < BOARD_SIZE and y >= 0 and y < BOARD_SIZE;
+}
+
+bool GoBoard :: newStep(int x, int y, Piece color){
+    if(!inside(x,  y) or grid[x][y] == Empty) return false;
+
+    grid[x][y] = color;
+    return true;
+}       
