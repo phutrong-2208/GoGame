8d6dede4cb8f104cbca48340c5d691499e34a52b
hxanonagujeno <nagujenodanhxano@gmail.com>
2025-10-20 08:56:12 +0700

    test3

--- include/mathFunctions.hpp
+++ include/mathFunctions.hpp
@@ -5,7 +5,7 @@
 #include <vector>
 
 namespace mathFunctions{
-    double Euclidean(double x1, double y1, double x2, double y2){
+    double Euclidean(double x1, double y1, double x2, double y2){ //Euclidean distance between two points
         x1 -= x2; y1 -= y2;
         return sqrtl(x1 * x1 + y1 * y1);
     }
